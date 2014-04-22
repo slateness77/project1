47ad598a651e9e44574efab745d5570e5865aba6
Rocky <slateness77@yahoo.com>
2014-04-22 15:30:45 -0700

    updated tic tac toe

--- tictactoe2/script.js
+++ tictactoe2/script.js
@@ -16,6 +16,7 @@ function TictactoeController($scope) {
 	 $scope.change = function() {
 	 	if ($scope.player === true) {
 	 		$scope.player = false;
+	 		
 	 	}
 	 	else {
 	 		$scope.player = true;
@@ -26,21 +27,54 @@ function TictactoeController($scope) {
 
 	 $scope.winFunction = function() {
 	 	if (($scope.board[0] === "X") && ($scope.board[1] === "X") && ($scope.board[2] === "X") ){
-	 		console.log("x wins");}
-	 		else if (($scope.board[3] === "X") && ($scope.board[4] === "X") && ($scope.board[5] === "X") ) {
-	 			console.log("X sucks");
+	 		console.log("X wins");}
+	 			else if (($scope.board[3] === "X") && ($scope.board[4] === "X") && ($scope.board[5] === "X") ) {
+	 			console.log("X Wins");}
 	 			else if (($scope.board[6] === "X") && ($scope.board[7] === "X") && ($scope.board[8] === "X") ) {
-	 			console.log("X sucks");
+	 			console.log("X Wins");}
 	 			else if (($scope.board[0] === "X") && ($scope.board[3] === "X") && ($scope.board[6] === "X") ) {
-	 			console.log("X sucks");
+	 			console.log("X Wins");}
 	 			else if (($scope.board[1] === "X") && ($scope.board[4] === "X") && ($scope.board[7] === "X") ) {
-	 			console.log("X sucks");
+	 			console.log("X Wins");}
 	 			else if (($scope.board[2] === "X") && ($scope.board[5] === "X") && ($scope.board[8] === "X") ) {
-	 			console.log("X sucks");
+	 			console.log("X Wins");}
+	 			else if (($scope.board[0] === "X") && ($scope.board[4] === "X") && ($scope.board[8] === "X") ) {
+	 			console.log("X Wins");}
+	 			else if (($scope.board[2] === "X") && ($scope.board[4] === "X") && ($scope.board[6] === "X") ) {
+	 			console.log("X Wins");}
+
+
+
+	 			else if (($scope.board[0] === "O") && ($scope.board[1] === "O") && ($scope.board[2] === "O") ) {
+	 			console.log("O Wins");}
+	 			else if (($scope.board[3] === "O") && ($scope.board[4] === "O") && ($scope.board[5] === "O") ) {
+	 			console.log("O Wins");}
+	 			else if (($scope.board[6] === "O") && ($scope.board[7] === "O") && ($scope.board[8] === "O") ) {
+	 			console.log("O Wins");}
+	 			else if (($scope.board[0] === "O") && ($scope.board[3] === "O") && ($scope.board[6] === "O") ) {
+	 			console.log("O Wins");}
+	 			else if (($scope.board[1] === "O") && ($scope.board[4] === "O") && ($scope.board[7] === "O") ) {
+	 			console.log("O Wins");}
+	 			else if (($scope.board[2] === "O") && ($scope.board[5] === "O") && ($scope.board[8] === "O") ) {
+	 			console.log("O Wins");}
+	 			else if (($scope.board[0] === "O") && ($scope.board[4] === "O") && ($scope.board[8] === "O") ) {
+	 			console.log("O Wins");}
+	 			else if (($scope.board[2] === "O") && ($scope.board[4] === "O") && ($scope.board[6] === "O") ) {
+	 			console.log("O Wins");}
+
+
+	 			$scope.xcell = function(index) {
+	 				return ($scope.board[index] === 'X');
+	 			};
+
+	 			$scope.ocell = function(index) {
+	 				return ($scope.board[index] === 'O');
+	 			};
+
 	 		
-	 	}
-	 	else {
-	 		$scope.change();
+	 	
+	 	// else {
+	 	// 	$scope.change();
 	 	}
 	 };
 
@@ -48,5 +82,5 @@ function TictactoeController($scope) {
 
 
 	
-}
+
 
